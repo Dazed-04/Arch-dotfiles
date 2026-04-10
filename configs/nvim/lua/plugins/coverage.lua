872711e1b87daa543d6c33cc7071683893372ac2
Dazed-04 <singhsuryansh150404@gmail.com>
2026-04-10 09:26:11 +0530

    automatic changes

--- configs/nvim/lua/plugins/coverage.lua
+++ configs/nvim/lua/plugins/coverage.lua
@@ -1,14 +1,17 @@
-return {
-  "andythigpen/nvim-coverage",
-  dependencies = "nvim-lua/plenary.nvim",
-  config = function()
-    require("coverage").setup({
-      auto_reload = true,
-      lang = {
-        go = {
-          coverage_file = "coverage.out",
-        },
-      },
-    })
-  end,
-}
+config = function()
+  require("coverage").setup({
+    auto_reload = true,
+    -- This is the part that enables full line highlights
+    highlights = {
+      covered = { fg = "#C3E88D" }, -- You can adjust colors to match Gruvbox
+      uncovered = { fg = "#F07178" },
+    },
+    -- Use this to ensure the signs (gutter) and highlights coexist
+    signs = {
+      covered = { hl = "CoverageCovered", text = "▎" },
+      uncovered = { hl = "CoverageUncovered", text = "▎" },
+    },
+    -- Enable line highlights
+    line_highlights = true,
+  })
+end
