324e55f5145ade15d5d36abbb10d626b8bc9a310
Dazed-04 <singhsuryansh150404@gmail.com>
2026-04-13 00:31:58 +0530

    automatic changes

--- configs/nvim/lua/config/lazy.lua
+++ configs/nvim/lua/config/lazy.lua
@@ -31,6 +31,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     -- Formatting Extra: Adds specialized formatting support
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- import/override with your plugins
--- user_scripts/utilities/bluetui.sh
+++ user_scripts/utilities/bluetui.sh
@@ -3,5 +3,6 @@
 if hyprctl clients | grep -q 'class: bluetui'; then
   hyprctl dispatch closewindow class:bluetui
 else
+  rfkill unblock bluetooth
   kitty --class bluetui bluetui
 fi
